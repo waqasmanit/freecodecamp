00a601fb2cb6f22a18b30e2f7d5f06475aa9e6c6
Owais Ahmad <INFORM.OS221@GMAIL.COM>
2023-05-03 14:27:41 +0530

    complete counter project

--- javaScript/counter/counter.js
+++ javaScript/counter/counter.js
@@ -1,3 +1,28 @@
-'use strict';
+"use strict";
+let counter = 0;
+let val1 = document.querySelector("#increase");
+let val2 = document.querySelector("#decrease");
+let val3 = document.querySelector("#reset")
+let digit = document.querySelector("#digit");
 
 
+//function for increase no. button
+//console.log("this", val);
+//console.log(typeof val);
+
+val1.addEventListener("click", () => {
+  counter++;
+  digit.textContent = counter;
+  //console.log(counter);
+});
+
+val2.addEventListener("click",() => {
+  counter--;
+  digit.textContent = counter;
+ // console.log(counter);
+} )
+
+val3.addEventListener('click',() => {
+    counter = 0;
+    digit.textContent = counter;
+} )
